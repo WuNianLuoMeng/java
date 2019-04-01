699db149eff5715ad880ce569d29d7e0838d24fe
chenhan Ma <1583724941@qq.com>
2019-04-02 00:39:02 +0800

    update

---
+++ 源代码/单例模式解析/singleclass1.java
@@ -0,0 +1,13 @@
+public class singleclass1 {
+​    public static void main(String[] args) {
+​        T x = T.getT();
+​    }
+}
+
+class T {
+​    private static T singleT = new T();
+​    public T(){}
+​    public static T getT() {
+​        return singleT;
+​    }
+}
