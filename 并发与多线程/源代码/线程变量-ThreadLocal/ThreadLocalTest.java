261dcb8ccbb1556ff8fa5da5c171ffd366d47374
chenhan Ma <1583724941@qq.com>
2019-04-04 17:35:58 +0800

    update

--- 并发与多线程/源代码/线程变量-ThreadLocal/ThreadLocalTest.java
+++ 并发与多线程/源代码/线程变量-ThreadLocal/ThreadLocalTest.java
@@ -1,24 +1,28 @@
 import java.lang.*;
 
 import com.sun.corba.se.impl.orbutil.StackImpl;
-public class ThreadLocalTest{
-    private static ThreadLocal<String> name=new ThreadLocal<>();
-    private static ThreadLocal<Long> id=new ThreadLocal<>();
-    public static String getName(){
+
+public class ThreadLocalTest {
+    private static ThreadLocal<String> name = new ThreadLocal<>();
+    private static ThreadLocal<Long> id = new ThreadLocal<>();
+
+    public static String getName() {
         return name.get();
     }
-    public static Long getId(){
+
+    public static Long getId() {
         return id.get();
     }
-    public static void main(String[] args){
-        Thread[] arr=new Thread[5];
-        for(int i=0;i<5;i++){
-            arr[i]=new Thread(()->{
+
+    public static void main(String[] args) {
+        Thread[] arr = new Thread[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = new Thread(() -> {
                 name.set(Thread.currentThread().getName());
                 id.set(Thread.currentThread().getId());
-                System.out.println("name:"+ThreadLocalTest.getName()+"  id:"+ThreadLocalTest.getId());
-                //System.out.println(name);
-                //System.out.println(id);
+                System.out.println("name:" + ThreadLocalTest.getName() + "  id:" + ThreadLocalTest.getId());
+                // System.out.println(name);
+                // System.out.println(id);
             });
             arr[i].start();
         }
